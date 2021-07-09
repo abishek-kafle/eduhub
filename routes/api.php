56b9fea06b366f4403d3ff7ce16404f3c497a52a
abishek kafle <abishek.kafle@gmail.com>
2021-07-09 13:14:53 +0545

    course api

--- routes/api.php
+++ routes/api.php
@@ -15,6 +15,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 Route::resource('course', CourseApiController::class);
+
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
